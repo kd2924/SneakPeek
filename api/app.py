4820c9fff2fa11fc1ffe9daa5164c66e41af9439
Kyle <kyledoodnauth2005@gmail.com>
2025-01-05 18:40:46 -0500

    update app.py

--- api/app.py
+++ api/app.py
@@ -10,42 +10,28 @@ load_dotenv()
 # Initialize Flask app
 app = Flask(__name__)
 
-# Enable CORS for your Flask app
-#CORS(app)
-CORS(app, resources={r"/*": {"origins": "https://sneak-peek-psi.vercel.app"}})
+# Enable CORS for Vercel domain
+CORS(app, resources={r"/*": {"origins": "https://your-vercel-frontend.vercel.app"}})
 
-
-# Home route
 @app.route('/')
 def home():
     return "Sneaker Backend is Running"
 
-# API route to get sneaker data
 @app.route('/api/sneakers/<sku>', methods=['GET'])
 def get_sneaker(sku):
-    # Trim any whitespace from SKU
     sku = sku.strip()
     product_url = f"https://stockx.com/{sku}"
     external_api_url = f"https://sneaker-database-stockx.p.rapidapi.com/searchByUrl?url={product_url}"
-
-    # Headers for RapidAPI request
+    
     headers = {
         'x-rapidapi-host': os.getenv('RAPIDAPI_HOST'),
         'x-rapidapi-key': os.getenv('RAPIDAPI_KEY')
     }
 
     try:
-        # Make a GET request to RapidAPI
         response = requests.get(external_api_url, headers=headers)
-        response.raise_for_status()  # Raise an error for bad responses (4xx/5xx)
+        response.raise_for_status()
         data = response.json()
-        print("API Response:", data)  # Debugging
-        return jsonify(data)  # Return the API response as JSON
+        return jsonify(data)
     except requests.exceptions.RequestException as e:
-        return jsonify({'error': str(e)}), 500  # Return the error message with status code 500
-
-
-
-# Run the Flask app
-if __name__ == '__main__':
-    app.run(debug=True)
+        return jsonify({'error': str(e)}), 500
